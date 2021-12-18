27da91d9397a2c96b67c5bd252fd43efc3c0881f
hi33741 <hi33741@naver.com>
2021-12-18 21:53:28 +0900

    eslint

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import AppLayout from "../component/AppLayout";
 const Home = () => {
-  return <AppLayout>asdf</AppLayout>;
+  return (
+    <AppLayout>
+      <div>Hello, Next!</div>
+    </AppLayout>
+  );
 };
 
 export default Home;
