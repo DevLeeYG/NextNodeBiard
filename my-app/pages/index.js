2f9f207b637b710624c2cbb04976e96d726f25c5
QooQua <hi06077@gmail.com>
2021-12-20 15:39:58 +0900

    client {date : 12/20 , 추가기능 : dummydata and postfrom build}

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,9 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import AppLayout from "../component/AppLayout";
+import PostForm from "../component/PostForm";
+import PostCard from "../component/PostCard";
 const Home = () => {
+  const { isLogin } = useSelector((state) => state.user);
+  const { mainPosts } = useSelector((state) => state.post);
+
   return (
     <AppLayout>
-      <div>Hello, Next!</div>
+      {isLogin && <PostForm />}
+      {mainPosts.map((post) => (
+        <PostCard key={post.id} post={post} />
+      ))}
     </AppLayout>
   );
 };
