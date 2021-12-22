a871e6ab98b12d8ebd03718004bca7226df9cce5
QooQua <hi06077@gmail.com>
2021-12-22 16:11:48 +0900

    client{date:12/22,개발기능:이미지 구현 }

--- my-app/component/PostImages.js
+++ my-app/component/PostImages.js
@@ -1,7 +1,69 @@
-import React from "react";
+import React, { useCallback } from "react";
 import PropTypes from "prop-types";
+import { PlusOutlined } from "@ant-design/icons";
 const PostImages = ({ images }) => {
-  return <div>구현중...</div>;
+  const onZoom = useCallback(() => {
+    setShowImagesZomm(true);
+  }, []);
+
+  if (images.length === 1) {
+    <>
+      <img
+        role="presentation"
+        src={images[0].src}
+        alt="이미지입니다"
+        onClick={onZoom}
+      />
+    </>;
+  }
+
+  if (images.length === 2) {
+    return (
+      <>
+        <img
+          role="presentation"
+          style={{ width: "50%", display: "inline-block" }}
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        <img
+          role="presentation"
+          width="50%"
+          style={{ width: "50%", display: "inline-block" }}
+          src={images[1].src}
+          alt={images[1].src}
+          onClick={onZoom}
+        />
+      </>
+    );
+  }
+
+  return (
+    <div>
+      <img
+        role="presentation"
+        style={{ width: "50%" }}
+        src={images[0].src}
+        alt={images[0]}
+        onClick={onZoom}
+      />
+      <div
+        role="presentation"
+        style={{
+          display: "inline-block",
+          width: "50",
+          textAlign: "center",
+          verticalAlign: "middle",
+        }}
+      >
+        <PlusOutlined />
+        <br />
+        {images.length - 1}
+        개의 사진 더보기
+      </div>
+    </div>
+  );
 };
 PostImages.propTypes = {
   images: PropTypes.arrayOf(PropTypes.object),
